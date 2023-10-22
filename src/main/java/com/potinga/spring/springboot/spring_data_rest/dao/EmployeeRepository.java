dc3c82ce8defb89c9d0c6a5d2c3d5c8e71b1a7de
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-10-22 20:29:58 +0300

    Create EmployeeRepository.java

---
+++ src/main/java/com/potinga/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.potinga.spring.springboot.spring_data_rest.dao;
+
+
+import com.potinga.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
